6831cef83f344b7f53efc6e708e73234e6b1e259
Yukari-World <yukariworld3154@hotmail.co.jp>
2024-02-03 11:31:54 +0900

    2024/02/03 Update Infomation
    Add:
    :sparkles: SEOデータの追加
    
    Modify:
    :arrow_up: packageの更新
    
    Other:
    :tada: 今日は二宮飛鳥さんの誕生日

--- app.vue
+++ app.vue
@@ -6,7 +6,23 @@ v-app#inspire
 </template>
 
 <script setup lang="ts">
-// null
+// ----------------------------------------------------------------------------------------------------
+// SEO Meta Data
+
+useSeoMeta({
+	title: 'Nuxt Technical Manual',
+	ogTitle: 'Nuxt Technical Manual',
+});
+
+
+// ----------------------------------------------------------------------------------------------------
+// Header Data
+
+useHead({
+	htmlAttrs: {
+		lang: 'ja',
+	},
+});
 </script>
 
 <script lang="ts">
